0c094dc4a7031a83557786914adcbdbcad4ac361
kirill864 <74341632+kirill864@users.noreply.github.com>
2024-05-15 20:48:52 +0300

    Update DBUtils.java

--- src/main/java/demo/fx_expenses/DBUtils.java
+++ src/main/java/demo/fx_expenses/DBUtils.java
@@ -19,104 +19,106 @@ import static java.sql.DriverManager.getConnection;
 public class DBUtils {
     private static Connection connection;
 
-    public static void setConnection(Connection conn) {
-        connection = conn;
-    }
+
+    // Метод для смены сцены
     public static <T> T changeScene(ActionEvent event, String fxmlFile, String title, String username) {
         Parent root = null;
         T controller = null;
 
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
+            FXMLLoader fxmlLoader = new FXMLLoader(DBUtils.class.getResource(fxmlFile)); // Загрузка FXML файла
             root = fxmlLoader.load();
-            controller = fxmlLoader.getController(); // Получаем контроллер
+            controller = fxmlLoader.getController(); // Получаем контроллер сцены
+
             if (controller instanceof AddTaskController) {
                 ((AddTaskController) controller).setConnection(connection); // Устанавливаем соединение
             }
+
             if (username != null) {
                 LoggedInController loggedInController = fxmlLoader.getController();
-                loggedInController.setUserInformation(username);
+                loggedInController.setUserInformation(username); // Устанавливаем имя пользователя
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle(title);
-        stage.setScene(new Scene(root));
-        stage.show();
+        stage.setTitle(title); // Устанавливаем заголовок сцены
+        stage.setScene(new Scene(root)); // Устанавливаем новую сцену
+        stage.show(); // Показываем сцену
 
-        return controller;
+        return controller; // Возвращаем контроллер
     }
 
-    public static void signUpUser(ActionEvent event, String username, String password){
+    // Метод для регистрации пользователя
+    public static void signUpUser(ActionEvent event, String username, String password) {
         Connection connection = null;
         PreparedStatement psInsert = null;
-        PreparedStatement psSchekuserExists = null;
+        PreparedStatement psCheckUserExists = null;
         ResultSet resultSet = null;
 
         try {
-            connection = getConnection("jdbc:mysql://localhost:3306/app_myexpenses", "root", "");
-            System.out.println("good connectiopn");
-            psSchekuserExists = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
-            psSchekuserExists.setString(1, username);
-            resultSet = psSchekuserExists.executeQuery();
+            connection = getConnection("jdbc:mysql://localhost:3306/app_myexpenses", "root", ""); // Подключение к базе данных
+            psCheckUserExists = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
+            psCheckUserExists.setString(1, username);
+            resultSet = psCheckUserExists.executeQuery();
 
-            if (resultSet.isBeforeFirst()){
+            if (resultSet.isBeforeFirst()) {
                 System.out.println("Такой пользователь уже есть");
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.show();
-            }else {
+            } else {
                 psInsert = connection.prepareStatement("INSERT INTO users (username, password) VALUES(?, ?)");
                 psInsert.setString(1, username);
                 psInsert.setString(2, password);
                 psInsert.executeUpdate();
 
-                changeScene(event, "logged-in.fxml", "Мои расходы", username);
-
+                changeScene(event, "logged-in.fxml", "Мои расходы", username); // Смена сцены после успешной регистрации
             }
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("bad ");
         } finally {
-            if (resultSet != null){
+            // Закрытие ресурсов
+            if (resultSet != null) {
                 try {
                     resultSet.close();
-                }catch (SQLException e ){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if (psSchekuserExists != null){
+            if (psCheckUserExists != null) {
                 try {
-                    psSchekuserExists.close();
-                }catch (SQLException e){
+                    psCheckUserExists.close();
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if (psInsert != null){
+            if (psInsert != null) {
                 try {
                     psInsert.close();
-                }catch (SQLException e){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if (connection != null){
+            if (connection != null) {
                 try {
                     connection.close();
-                }catch (SQLException e){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
 
+    // Метод для входа в систему
     public static void logInUser(ActionEvent event, String username, String password) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
         try {
-            connection = getConnection("jdbc:mysql://localhost:3306/app_myexpenses", "root", "");
+            connection = getConnection("jdbc:mysql://localhost:3306/app_myexpenses", "root", ""); // Подключение к базе данных
             preparedStatement = connection.prepareStatement("select password from users where username = ?");
             preparedStatement.setString(1, username);
             resultSet = preparedStatement.executeQuery();
@@ -130,7 +132,7 @@ public class DBUtils {
                     String retrievedPassword = resultSet.getString("password");
                     if (retrievedPassword.equals(password)) {
                         App.loggedInUsername = username; // Сохраняем имя пользователя
-                        changeScene(event, "logged-in.fxml", "2DO", username);
+                        changeScene(event, "logged-in.fxml", "2DO", username); // Смена сцены после успешного входа
                     } else {
                         System.out.println("Неправильный пароль");
                         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -141,6 +143,7 @@ public class DBUtils {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
+            // Закрытие соединения с базой
             if (resultSet != null) {
                 try {
                     resultSet.close();
